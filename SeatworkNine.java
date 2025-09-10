464f8e12ab1371e4a9b62e7c0f5854c9bd3152dc
Johanzisabusybaldguy <jelsongulas.scc@gmail.com>
2025-09-10 22:17:40 +0800

    9

---
+++ SeatworkNine.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main (String [] args) {
+    System.out.println("SeatworkNine");
+
+    int [] numbers = {10, 20, 30, 40, 50};
+    int index = 0;
+
+    while (index < 5) {
+      System.out.print("value of item : " + numbers[index]);
+      index++;
+      System.out.print("\n");
+    }
+  }
+}
